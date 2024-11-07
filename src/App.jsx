6c53e2c7ce8a5e4cb443e77b2cf22b5d7ca9841d
Nicole Riera <150852807+NicoRi5@users.noreply.github.com>
2024-11-06 19:12:32 -0500

    update error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { useState } from "react";
-import ContactList from "./components//ContactList.jsx";
+import ContactList from "./components/ContactList.jsx";
 import SelectedContact from "./components/SelectedContact";
 
 export default function App() {
